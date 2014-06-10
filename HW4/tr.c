5a795dbc60b583f235d8a4e9556c68d22b7a702d
thebrandontyler <brandontyler@me.com>
2014-06-09 17:44:58 -0700

    added tr.c and changed print value from 1000 to 10
    for testing purposes

---
+++ HW4/tr.c
@@ -0,0 +1,45 @@
+
+/* read-only version of t.c             */
+/* for testing HW4, ecs150, winter 2010 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main
+(int argc, char *argv[])
+{
+  FILE *target = NULL;
+  int i, value;
+
+  if (argc != 2)
+    {
+      fprintf(stderr, "usage: $ %s <downloading-snoopfs-file-name>\n",
+	      argv[0]);
+      exit(0);
+    }
+
+  target = fopen(argv[1], "r");
+  if (target == NULL)
+    {
+      fprintf(stderr, "$ %s %s File open error!\n",
+	      argv[0], argv[1]);
+      exit(-1);
+    }
+
+  for (i = 0; i < 10; i++)
+    {
+		int ran = rand();
+      fseek(target, ran, SEEK_SET);
+		printf("tr.c random number : %d\n", ran);
+      fscanf(target, "%d", &value);
+      if (i % 100 == 0)
+	{
+	  fprintf(stderr, ".");
+	  sleep(1);
+	}
+    }
+  fprintf(stderr, "\n");
+  fclose(target);
+  return 0;
+}
